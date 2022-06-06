4a9810d575e9490a82b16e71c0963ea98237873d
Rodrigo Medina <rodrigom@lyft.com>
2022-06-05 20:55:48 -0500

    Adding main subcommands

---
+++ cmd/status.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// statusCmd represents the status command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Get the status of a challenge submission",
+	Long: `Get the status of a challenge submission by its submission ID.
+
+Based on the submission ID of the submission we can fetch the status from it,
+and you can choose to wait until the execution has finished, or just get the current
+status as it is.
+
+A execution is considered finished when the status is neither "In Queue" or "Processing".`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("status called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// statusCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/submit.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// submitCmd represents the submit command
+var submitCmd = &cobra.Command{
+	Use:   "submit",
+	Short: "Submit local files from specified directory to Judge0",
+	Long: `Submit local coding challenge files from specified directory to the Judge0 server.
+
+We're expecting that the directory contains the following files:
+- run (bash script)
+- main*
+- test*
+- testframework*
+
+* This files should have the same extension.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("submit called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(submitCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// submitCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// submitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
